d4f909abd9d46f21f704d1af0024fbaf29e4ac6b
khadij3131 <khadij-32@hotmail.com>
2023-12-07 12:06:04 -0800

    print

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,17 +1,13 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LIST_H
+#define LIST_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- *
  * Description: doubly linked list node structure
- * 
  */
 typedef struct dlistint_s
 {
@@ -29,4 +25,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-#endif 
+
+#endif/*LIST_H*/ 
